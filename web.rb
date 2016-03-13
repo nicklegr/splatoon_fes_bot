f57dcb1b1e0690e2e9094a4086bcedf5fdc1868d
nicklegr <addr.for+github@gmail.com>
2016-03-13 15:13:34 +0900

    勝手にフェスパワーランキング

--- web.rb
+++ web.rb
@@ -19,3 +19,42 @@ get "/" do
 
   slim :index
 end
+
+get "/ranking" do
+  config = YAML.load_file('config.yaml')
+
+  start_time = Time.parse(config['fes_period']['start'])
+  end_time = Time.parse(config['fes_period']['end'])
+
+  tweets = Tweet
+    .where(:created_at => { '$gte' => end_time })
+    .and({ :text => /パワーを記録したぜ！/ })
+    .and({ :text => %r|https://t\.co/| })
+
+  powers = tweets.map do |e|
+    e.text =~ /(\d+)(ガンガン|だいじに)パワー/
+    power_text = $&
+    power = $1.to_i
+
+    e.text =~ %r|https://t\.co/\w+|
+    url = $&
+
+    {
+      :screen_name => e.screen_name,
+      :text => e.text,
+      :power => power,
+      :power_text => power_text,
+      :url => url,
+    }
+  end
+
+  users = powers.group_by{ |e| e[:screen_name] }
+
+  @ranking = users.map do |k, v|
+    v.max_by{ |e| e[:power] }
+  end
+
+  @ranking = @ranking.sort_by{ |e| e[:power] }.reverse
+
+  slim :ranking
+end
